0d4172096f247bf307acf81bbcdfb4661ee5dc6e
tuhbm <tuhbm2@gmail.com>
2018-08-28 16:23:42 +0900

    ejs추가 및 몽고디비세팅

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 var createError = require('http-errors');
+// 몽구스 ODM
+var mongoose = require('mongoose');
+// 세션 저장용 모듈
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+// 패스포트와 경고 플래시 메시지 모듈 가져오기
+var passport = require('passport');
+var flash = require('connect-flash');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -11,45 +19,76 @@ var usersRouter = require('./server/routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'server/views'));
+app.set('views', path.join(__dirname, 'server/views/pages'));
 app.set('view engine', 'ejs');
+
+// 데이터베이스 설정
+var config = require('./server/config/config.js');
+// 데이터베이스 연결
+mongoose.connect(config.url);
+// 몽고DB가 실행 중인지 체크
+mongoose.connection.on('error', function() {
+    console.error('MongoDB Connection Error. Make sure MongoDB is running.')
+});
+// 패스포트 설정
+require('./server/config/passport')(passport);
+
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    indentedSyntax: true, // true = .sass and false = .scss
+    sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// 패스포트용
+// 세션용 비밀키
+app.user(session({
+    secret: 'sometextgohere',
+    saveUninitialized: true,
+    resave: true,
+    // express-session과 connect-mongo를 이용해 몽고DB에 세션 저장
+    store: new MongoStore({
+        url: config.url,
+        collection: 'sessions'
+    })
+}));
+// 패스포트 인증 초기화
+app.use(passport.initialize());
+// 영구적인 로그인 세션
+app.use(passport.session());
+// 플래시 메시지
+app.use(flash());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
 
-app.set('post', process.env.PORT || 3000);
-var server = app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + server.address().port);
+app.set('port', process.env.PORT || 3000);
+var server = app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + server.address().port);
 });
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express from server folder' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', {title: 'Login Page', message: req.flash('loginMessage')});
+});
+
+router.get('/signup', function(req, res) {
+  res.render('signup', {title: 'Signup Page', message: req.flash('signupMessage')});
+});
+
+router.get('/profile', function(req, res, next) {
+  res.render('profile', {title: 'Profile Page', message: req.user, avator: gravatar.url(req.user.email, {s: '100', r:'x', d: 'retro'}, true)});
+});
+
 module.exports = router;
